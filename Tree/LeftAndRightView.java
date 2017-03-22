3b0b7750b4b5c5955ab4ace4a5598b4ba94703c3
tejashricp <tejashricpathrikar@gmail.com>
2017-03-22 16:31:02 +0530

    Update LeftAndRightView.java

--- Tree/LeftAndRightView.java
+++ Tree/LeftAndRightView.java
@@ -2,61 +2,93 @@
 *Given a binary tree, print the left and right view of this binary tree using two tree traversals.
 * */
 
-public class Demo {
-    public static int count = 1;
+/*
+*  Left and right view of a tree traversal
+* */
 
+
+
+import sun.reflect.generics.tree.Tree;
+
+import java.util.*;
+
+public class Demo {
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(0);
-        TreeNode a1 = new TreeNode(1);
-        TreeNode a2 = new TreeNode(2);
-        TreeNode a3 = new TreeNode(3);
-        TreeNode a4 = new TreeNode(4);
-        TreeNode a5 = new TreeNode(5);
-        TreeNode a6 = new TreeNode(6);
+        TreeNode root =  new TreeNode(1);
+        TreeNode a1 = new TreeNode(2);
+        TreeNode a2 = new TreeNode(3);
+        TreeNode a3 = new TreeNode(4);
+        TreeNode a4 = new TreeNode(5);
+        TreeNode a5 = new TreeNode(6);
+        TreeNode a6 = new TreeNode(7);
+        TreeNode a7 = new TreeNode(8);
 
         root.left = a1;
-        root.right = a2;
+        root.right =a2;
 
         a1.left = a3;
         a1.right = a4;
 
+
         a2.left = a5;
-        a3.left = a6;
+        a2.right = a6;
 
-        printAllEdgeNodes(root);
-
+        a4.left = a7;
+        Queue<TreeNode> treeNodeQueueRead = new LinkedList<>();
+        Queue<TreeNode> treeNodeQueueCopy = new LinkedList<>();
+        treeNodeQueueRead.add(root);
+        leftViewTraverse(treeNodeQueueCopy,treeNodeQueueRead);
+        System.out.println("===================");
+        treeNodeQueueCopy.clear();
+        treeNodeQueueRead.clear();
+        treeNodeQueueRead.add(root);
+        rightViewTraverse(treeNodeQueueCopy,treeNodeQueueRead);
     }
 
-    private static void printAllEdgeNodes(TreeNode root) {
-        printAllLeftEdges(root);
-        printAllRightEdges(root);
-
+    private static void leftViewTraverse(Queue<TreeNode> treeNodeQueueCopy, Queue<TreeNode> treeNodeQueueRead) {
+            if(treeNodeQueueRead.isEmpty()) 
+                return;
+            TreeNode root = treeNodeQueueRead.poll();
+            if(root.right != null)
+                treeNodeQueueCopy.add(root.right);
+            if(root.left != null)
+                treeNodeQueueCopy.add(root.left);
+            if(treeNodeQueueRead.isEmpty()) {
+                System.out.print(root.data+" ");
+                while (!treeNodeQueueCopy.isEmpty())
+                    treeNodeQueueRead.add(treeNodeQueueCopy.poll());
+                System.out.println();
+            }
+        leftViewTraverse(treeNodeQueueCopy,treeNodeQueueRead);
     }
 
-    private static void printAllRightEdges(TreeNode root) {
-        if(root == null)
+    private static void rightViewTraverse(Queue<TreeNode> treeNodeQueueCopy, Queue<TreeNode> treeNodeQueueRead) {
+        if(treeNodeQueueRead.isEmpty()) {
             return;
-        System.out.println(root.data);
-        printAllRightEdges(root.right);
+        }
+        TreeNode root = treeNodeQueueRead.poll();
+        if(root.left != null)
+            treeNodeQueueCopy.add(root.left);
+        if(root.right != null)
+            treeNodeQueueCopy.add(root.right);
+        if(treeNodeQueueRead.isEmpty()) {
+            System.out.print(root.data+" ");
+            while (!treeNodeQueueCopy.isEmpty())
+                treeNodeQueueRead.add(treeNodeQueueCopy.poll());
+            System.out.println();
+        }
+        rightViewTraverse(treeNodeQueueCopy,treeNodeQueueRead);
     }
 
-    private static void printAllLeftEdges(TreeNode root) {
-        if(root == null)
-            return;
-        System.out.println(root.data);
-        printAllLeftEdges(root.left);
 
+    public static class TreeNode {
+        int data;
+        TreeNode left ;
+        TreeNode right;
+
+        public TreeNode(int data) {
+            this.data = data;
+        }
     }
 
-
-}
-
-class TreeNode {
-    int data;
-    TreeNode left;
-    TreeNode right;
-
-    public TreeNode(int data) {
-        this.data = data;
-    }
 }
