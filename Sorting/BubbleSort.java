2d4c20285e02b2ca0aa5ec25f5ab570b8606b315
teja <tejashricpathrikar@gmail.com>
2017-08-10 18:50:07 +0530

    Create BubbleSort.java

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,45 @@
+/**
+ * Created by TPathrikar on 8/3/2017.
+ Bubble sort
+ i/p:7 2 16 1 4 5
+ o/p:1 2 4  5 7 16
+ * this is inplace sorting as we are using same array space
+ * at a time compares betn current number and next element
+ *in this way in one pass u get largest elemnt at the end
+ * this is the best algo to sort an array which has only one
+ * element out of place
+ * */
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        int[] arr = {7, 2, 16, 1, 4, 5};
+        doBubbleSort(arr);
+        for(int i=0;i<arr.length;i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    private static void doBubbleSort(int[] arr) {
+        boolean swap = true;
+        int k = arr.length-1;
+        while(swap) {
+            swap = false;
+            for(int i=0;i<=k-1;i++){
+                if(arr[i]>arr[i+1]) {
+                    swap(arr, i, i + 1);
+                    swap = true;
+                }
+            }
+            k--;
+        }
+    }
+
+    private static void swap(int[] arr, int min, int index) {
+        int temp = arr[min];
+        arr[min] = arr[index];
+        arr[index] = temp;
+    }
+
+
+}
