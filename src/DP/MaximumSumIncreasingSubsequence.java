59d24114681bdf8123a2f2360ce8d16babc61830
SABA\TPathrikar <tejashricpathrikar@gmail.com>
2017-08-20 16:18:34 +0530

    Changing resurcive solution to DP

--- src/DP/MaximumSumIncreasingSubsequence.java
+++ src/DP/MaximumSumIncreasingSubsequence.java
@@ -1,5 +1,8 @@
 package DP;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by TPathrikar on 8/20/2017.
  */
@@ -17,30 +20,38 @@ package DP;
 public class MaximumSumIncreasingSubsequence {
     public static void main(String[] args) {
         int[] arr = {1, 101, 2, 3, 100, 4, 5};
-        int maxSum = findMaxSumSubsequence(arr,0,arr[0]);
+        int[] temp = new int[arr.length];
+        Arrays.fill(temp,-1);
+        int maxSum = findMaxSumSubsequence(arr,0,arr[0],temp);
+
         System.out.println(maxSum);
         int[] arr1 = {3, 4, 5, 10};
-        maxSum = findMaxSumSubsequence(arr1,0,arr1[0]);
+        int[] temp1 = new int[arr1.length];
+        Arrays.fill(temp1,-1);
+        maxSum = findMaxSumSubsequence(arr1,0,arr1[0],temp1);
         System.out.println(maxSum);
     }
 
-    private static int findMaxSumSubsequence(int[] arr,int current,int lastNumber) {
+    private static int findMaxSumSubsequence(int[] arr,int current,int lastNumber,int[] temp) {
         if(current>=arr.length)
             return 0;
         if(arr[current]<lastNumber)
-            return findMaxSumSubsequence(arr,current+1,lastNumber);
-
-        if(arr[current]>lastNumber)
-            return arr[current]+findMaxSumSubsequence(arr,current+1,arr[current]);
+            return findMaxSumSubsequence(arr, current + 1, lastNumber,temp);
+        if(arr[current]>lastNumber && temp[current] != -1)
+            return temp[current];
+        if(arr[current]>lastNumber) {
+            temp[current] = arr[current] + findMaxSumSubsequence(arr, current + 1, arr[current],temp);
+            return temp[current];
+        }
         int max = Integer.MIN_VALUE;
-
         for (int i=0;i<arr.length;i++ ) {
             for(int j=i+1;j<arr.length;j++) {
-                int localMax = arr[i] + findMaxSumSubsequence(arr, j, arr[i]);
-                if (localMax > max)
+                int localMax = arr[i] + findMaxSumSubsequence(arr, j, arr[i],temp);
+                if (localMax > max) {
+                    temp[i] = localMax;
                     max = localMax;
+                }
             }
-
         }
         return max;
     }
