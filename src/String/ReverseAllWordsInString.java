0c27f3d41bef62596a605d61b40febc7c25e8e43
SABA\TPathrikar <tejashricpathrikar@gmail.com>
2017-08-30 23:59:52 +0530

    Reverse words separated by dot in string

--- src/String/ReverseAllWordsInString.java
+++ src/String/ReverseAllWordsInString.java
@@ -34,12 +34,43 @@ mno.pqr
 public class ReverseAllWordsInString {
     public static void main(String[] args) {
         String input = "i.like.this.program.very.much";
-        String out = reverseString(input);
+        reverseString(input);
+        reverseString("mno.pqr");
     }
 
     private static String reverseString(String input) {
-
-
+        int i = 0;
+        int j = input.length()-1;
+        char[] charArr = input.toCharArray();
+        while (i<j){
+            char temp = charArr[i];
+            charArr[i] = charArr[j];
+            charArr[j] = temp;
+            i++;
+            j--;
+        }
+        i = 0;
+        j = 0;
+        while (j != charArr.length){
+            if(charArr[j] == '.' && j != i) {
+                reverseWord(charArr, i, j-1);
+                i = j+1;
+            }
+                j++;
+        }
+        if(i != j)
+            reverseWord(charArr,i,j-1);
+        System.out.println(String.valueOf(charArr));
         return null;
+    }
+
+    private static void reverseWord(char[] charArr, int i, int j) {
+        while (i<j){
+            char temp = charArr[i];
+            charArr[i] = charArr[j];
+            charArr[j] = temp;
+            i++;
+            j--;
+        }
     }
 }
