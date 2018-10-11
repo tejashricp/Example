e2011ed003e71297177e6303442418b03ba9ef0b
Tejashri Pathrikar <tejashri_chandrashekhar_pathrikar@external.mckinsey.com>
2018-10-11 17:06:31 +0530

    Logic added to find kth element

--- src/Array/KthElementInTwoSortedArrays.java
+++ src/Array/KthElementInTwoSortedArrays.java
@@ -1,6 +1,5 @@
 package Array;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class KthElementInTwoSortedArrays {
@@ -8,18 +7,18 @@ public class KthElementInTwoSortedArrays {
     public static void main(String[] args) {
         int arr1[] = {12, 34, 2, 5, 123, 78};
         int arr2[] = {98, 54, 33, 7, 2, 90};
-        int k = 5;
+        int k = 8;
         Arrays.sort(arr1);
         Arrays.sort(arr2);
-        findKthElement(arr1, arr2, k);
-        System.out.println();
+
+        System.out.println(findKthElement(arr1, arr2, k));
     }
 
     // iterate from last element of arr2 and compare with last element of arr1
     // find correct place of arr2 element in arr1 if less that last element of arr1
     // cache last element of arr1 and move all elements till the correct place to right in arr1
     // replace add arr2 element in arr1 correct place and arr1 last element in arr2 element's place
-    private static void findKthElement(int[] arr1, int[] arr2, int k) {
+    private static int findKthElement(int[] arr1, int[] arr2, int k) {
         int j = arr2.length-1, i = arr1.length-1;
         int m = arr1.length-1;
         for(;j>=0;j--){
@@ -32,5 +31,9 @@ public class KthElementInTwoSortedArrays {
                 arr2[j] = last;
                 m = arr1.length-1;
         }
+        if(k-1 < arr1.length)
+            return arr1[k-1];
+        else
+            return arr2[(k-arr1.length)-1];
     }
 }
