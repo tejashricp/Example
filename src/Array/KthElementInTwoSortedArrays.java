95f845ff8883b6a19e7748c4b1960bc045350d1a
Tejashri Pathrikar <tejashri_chandrashekhar_pathrikar@external.mckinsey.com>
2018-10-11 17:00:32 +0530

    Working solution with changed approch of iterating from last elements in both the arrays

--- src/Array/KthElementInTwoSortedArrays.java
+++ src/Array/KthElementInTwoSortedArrays.java
@@ -15,25 +15,22 @@ public class KthElementInTwoSortedArrays {
         System.out.println();
     }
 
+    // iterate from last element of arr2 and compare with last element of arr1
+    // find correct place of arr2 element in arr1 if less that last element of arr1
+    // cache last element of arr1 and move all elements till the correct place to right in arr1
+    // replace add arr2 element in arr1 correct place and arr1 last element in arr2 element's place
     private static void findKthElement(int[] arr1, int[] arr2, int k) {
-        int i = 0, j = 0;
-        while (i < arr1.length && j < arr2.length) {
-            if (arr1[i] == arr2[j]) {
-                i++;
-            } else if (arr1[i] < arr2[j])
-                i++;
-            else if (arr1[i] > arr2[j]) {
-                int temp = arr1[i];
-                arr1[i] = arr2[j];
-                if(arr2[j+1] >= temp){
-                    i++;
-                    arr2[j] = temp;
-                    j++;
+        int j = arr2.length-1, i = arr1.length-1;
+        int m = arr1.length-1;
+        for(;j>=0;j--){
+                int last = arr1[i];
+                while (arr2[j]<arr1[m] && arr2[j] < arr1[m-1] && m > 0){
+                    arr1[m] = arr1[m-1];
+                    m--;
                 }
-                else {
-                    j++;
-                }
-            }
+                arr1[m] = arr2[j];
+                arr2[j] = last;
+                m = arr1.length-1;
         }
     }
 }
